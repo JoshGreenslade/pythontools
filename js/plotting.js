e2032b1f9ea0dc6d37c512e3807ad969060c0f2f
Joshua Greenslade <joshua.greenslade@onyxinsight.com>
2023-08-29 17:03:57 +0100

    More learning

--- js/plotting.js
+++ js/plotting.js
@@ -1,12 +1,14 @@
 const aspect_ratio = 8.0/3.0
-const height= 300
+const height= 250
 const width= height * aspect_ratio
+const margin = 30
 
 var svg = d3.select(".area")
   .append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .style("background-color","red")
+  .attr("width", width + 2*margin)
+  .attr("height", height + 2*margin)
+  .append("g")
+    .attr("transform", `translate(${margin}, ${margin})`)
 
 var x = d3.scaleLinear()
   .domain([0, width])
@@ -16,9 +18,24 @@ var y = d3.scaleLinear()
   .domain([0, height])
   .range([0, height])
 
-svg.append('g')
+var d3xAxis = d3.axisTop(x)
+  .tickValues([])
+  .tickSize(0);
+
+
+var d3yAxis = d3.axisLeft(y)
+  .tickValues([])
+  .tickSize(0);
+
+const xAxis = svg.append('g')
   .attr("transform", `translate(0, ${height})`)
-  .call(d3.axisTop(x));
-svg.append('g').call(d3.axisRight(y));
+  .call(d3xAxis)
+
+const yAxis = svg.append('g')
+  .call(d3yAxis);
+
+xAxis.attr("style", "color: hsl(0, 0%, 50%); stroke-width: 2px;")
+yAxis.attr("style", "color: hsl(0, 0%, 50%); stroke-width: 2px;")
+
 
 console.log('hello')
